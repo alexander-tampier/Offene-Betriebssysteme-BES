6c759f46b814d2387a4b2d5ad8f3195e88ce8667
Alexander Tampier <alexander.tampier@icloud.com>
2018-06-23 14:20:25 +0200

    extended myFind with capitalization method

--- Uebung01/main.c
+++ Uebung01/main.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 #include <getopt.h>
 #include <stdlib.h>
+#include <string.h>
 
+void handleMyFind(char *searchPath[], char *filesVar[], int recursiveFlag, int ignoreCapitalization, int n);
+
+char* convertCapitalLetters(char toLower[]);
 
 int main(int argc, char **argv) {
     int c;
-    int Recursive = 0;
+    int recursive = 0;
     int ignoreCapitalization = 0;
-    char *path[100];
+    char *searchPath[100];
 
     while ((c = getopt(argc, argv, "iR")) != -1)
         switch (c) {
             case 'R':
-                Recursive = 1;
-                printf("-R\n");
+                recursive = 1;
                 break;
             case 'i':
                 ignoreCapitalization = 1;
-                printf("-i\n");
                 break;
             default:
                 fprintf(stderr, "Usage: %s [-R] [-i] searchpath filename1 [filename2] ...[filenamen]\n",
@@ -25,15 +27,46 @@ int main(int argc, char **argv) {
                 exit(EXIT_FAILURE);
         }
 
-    printf("name argument = %s\n", argv[optind]);
-    *path=argv[optind];
+    *searchPath = argv[optind];
 
-    char* files[argc-optind+1];
+    char *files[argc - (optind + 1)];
 
-    for(int i=optind+1;i<argc;i++){
-        files[i] = argv[i];
-        printf("%s\n", files[i]);
+    int index = optind + 1;
+    int n = sizeof(files) / sizeof(files[0]);
+    for (int i = 0; i < n; i++) {
+        files[i] = argv[index];
+        index++;
     }
 
+    handleMyFind(&searchPath, &files, recursive, ignoreCapitalization, n);
+
     return 0;
+}
+
+void handleMyFind(char *searchPath[], char *filesVar[], int recursiveFlag, int ignoreCapitalization, int n) {
+    if (ignoreCapitalization != 0) {
+        char testString[] = "CapitALleTTERtest";
+        *testString = convertCapitalLetters(testString);
+    }
+
+    printf("Searchpath: %s\n", *searchPath);
+
+    for (int i = 0; i < n; i++) {
+        printf("%s\n", filesVar[i]);
+    }
+
+}
+
+char* convertCapitalLetters(char toLower[]) {
+    for (int i = 0; i < strlen(toLower); i++) {
+        unsigned int asciiNum = toLower[i];
+        if (asciiNum >= 65 && asciiNum <= 90) {
+            toLower[i] += (97 - 65);
+        }
+        printf("%c", toLower[i]);
+    }
+
+    printf("\n");
+
+    return toLower;
 }
