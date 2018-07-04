261ca9be9e3d84e3ea8736619b8e5eb51b099974
Alexander Tampier <alexander.tampier@icloud.com>
2018-07-04 22:23:02 +0200

    implemented recursive search

--- Uebung01/main.c
+++ Uebung01/main.c
@@ -6,13 +6,73 @@
 #include <string.h>
 #include <pthread.h>
 
-int main(int argc, char **argv) {
-    pthread_mutex_t mutex;
+int
+searchFiles(const char *searchPath, int localRecursive, int localIgnoreCapitalization, char *myLocalFileToFind) {
+    //struct for directory operations
+    struct dirent *direntp;
+    DIR *dirp;
 
+    //errorhandling directory open
+    if (!(dirp = opendir(searchPath))) {
+        perror("Failed to open directory");
+        return 0;
+    }
+
+
+    //dir opened, while until EOF dir
+    //readdir -> reads next/current file
+    while ((direntp = readdir(dirp)) != NULL) {
+        //if -i is true (strCASEcmp)
+        if (localIgnoreCapitalization != 0) {
+
+            //comparing local file to find with the opened directories files
+            if (strcasecmp(direntp->d_name, myLocalFileToFind) == 0) {
+
+                //mutex lock for printf (outputstream)
+                printf("++Process: %d -> Found File directory: %s/%s\n", getpid(), searchPath,
+                       direntp->d_name);
+                //flush to delete remaining buffer of outputstream
+                fflush(stdout);
+                //printf("%d:%s:%s%s\n\n",getpid(), direntp->d_name, myLocalSearchPath, direntp->d_name);
+                //wait for file close
+                while ((closedir(dirp) == -1) && (errno == EINTR));
+
+                return 0;
+            }
+
+            //same without -i (not true)
+        } else if (strcmp(myLocalFileToFind, direntp->d_name) == 0) {
+            printf("++Process: %d -> Found File directory: %s/%s\n", getpid(), searchPath,
+                   direntp->d_name);
+            //flush to delete remaining buffer of outputstream
+            fflush(stdout);
+            //printf("%d:%s:%s%s\n\n",getpid(), direntp->d_name, myLocalSearchPath, direntp->d_name);
+            //wait for file close
+            while ((closedir(dirp) == -1) && (errno == EINTR));
+
+            return 0;
+        }
+
+        if (direntp->d_type == DT_DIR && localRecursive && direntp->d_name[0] != '.' && direntp->d_name[1] != '.') {
+            char newPath[1024];
+            //strcpy(newPath, direntp->d_name);
+            //printf("Hello World: %s\n", newPath);
+            snprintf(newPath, sizeof(newPath), "%s/%s", searchPath, direntp->d_name);
+            return searchFiles(newPath, localRecursive, localIgnoreCapitalization, myLocalFileToFind);
+
+        }
+
+    }
+    //wait for file close
+    while ((closedir(dirp) == -1) && (errno == EINTR));
+
+    return 0;
+}
+
+int main(int argc, char **argv) {
     int c;
     int recursive = 0;
     int ignoreCapitalization = 0;
-    char *searchPath[100];
 
     //read the parameter with a minus form command
     while ((c = getopt(argc, argv, "iR")) != EOF)
@@ -29,9 +89,7 @@ int main(int argc, char **argv) {
                 exit(EXIT_FAILURE);
         }
 
-        //optind is int after the parameters (when other arguments start)
-    *searchPath = argv[optind];
-//files to search (argc is all option parameters incl programm name)
+    //files to search (argc is all option parameters incl programm name)
     char *files[argc - (optind + 1)];
 
     //initialize array of files with given parameter(files)
@@ -46,72 +104,39 @@ int main(int argc, char **argv) {
     pid_t pid;
 
     //iterate through files and start X child processes
-    for(int i=0;i<n;i++) {
+    for (int i = 0; i < n; i++) {
         pid = fork();
 
         if (pid == 0) {
             /* Kindprozess
              * wenn fork() eine 0 zurückgibt, befinden wir uns im Kindprozess
              */
-            char myLocalSearchPath[100];
-            char myLocalFileToFind[100];
+            char myLocalFileToFind[1024];
 
-            //struct for directory operations
-            struct dirent *direntp;
-            DIR *dirp;
+
 
             //mutexes locked and unlocked while children could try to access same
             //resources at the SAME time, so they save them locally
-            pthread_mutex_lock(&mutex);
-            strcpy(myLocalSearchPath, *searchPath);
             strcpy(myLocalFileToFind, files[i]);
             int localIgnoreCapitalization = ignoreCapitalization;
-            pthread_mutex_unlock(&mutex);
+            int localRecursive = recursive;
 
-            //errorhandling directory open
-            if ((dirp = opendir(*searchPath)) == NULL) {
-                perror("Failed to open directory");
-                return 1;
-            }
 
-            //dir opened, while until EOF dir
-                    //readdir -> reads next/current file
-            while ((direntp = readdir(dirp)) != NULL) {
-                //if -i is true (strCASEcmp)
-                if (localIgnoreCapitalization != 0) {
+            //remove slash
+            char *p = argv[optind];
+            p[strlen(argv[optind]) - 1] = 0;
+            //printf("p: %s\n", p);
 
-                    //comparing local file to find with the opened directories files
-                    if (strcasecmp(direntp->d_name, myLocalFileToFind) == 0) {
+            return searchFiles(p, localRecursive, localIgnoreCapitalization, myLocalFileToFind );
 
-                        //mutex lock for printf (outputstream)
-                        pthread_mutex_lock(&mutex);
-                        printf("++Process: %d -> Found File directory: %s%s\n", getpid(), myLocalSearchPath,
-                               direntp->d_name);
-                        printf("%d:%s:%s%s\n\n",getpid(), direntp->d_name, myLocalSearchPath, direntp->d_name);
-                        pthread_mutex_unlock(&mutex);
-                    }
 
-                    //same without -i (not true)
-                } else if (strcmp(myLocalFileToFind, direntp->d_name) == 0) {
-                    pthread_mutex_lock(&mutex);
-                    printf("++Process: %d -> Found File directory: %s%s\n", getpid(), myLocalSearchPath,
-                           direntp->d_name);
-                    printf("%d:%s:%s%s\n\n",getpid(), direntp->d_name, myLocalSearchPath, direntp->d_name);
-                    pthread_mutex_unlock(&mutex);
-                }
-            }
-            //wait for file close
-            while ((closedir(dirp) == -1) && (errno == EINTR));
-            //flush to delete remaining buffer of outputstream
-            fflush(stdout);
-            exit(0);
         } else if (pid > 0) {
-            fflush(stdout);
         }
     }
     //wait for all children - asynchron
+    sleep(1);
     while (wait(NULL) > 0) {}
-    printf("Parentid: %d\n",getpid());
+    printf("Parentid: %d\n", getpid());
     printf("WAITED");
 
     return 0;
